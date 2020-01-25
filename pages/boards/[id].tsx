2b7a772d0c3b56e3f169c5dc8f893ab910b49e5f
KIMINJOONG <ijboym216@gmail.com>
2020-01-25 14:37:07 +0900

    i try to make to board detail layout

--- pages/boards/[id].tsx
+++ pages/boards/[id].tsx
@@ -1,9 +1,56 @@
-import Router, { useRouter } from "next/router";
+import { useRouter } from "next/router";
+import {
+  makeStyles,
+  createStyles,
+  Theme,
+  Grid,
+  Typography
+} from "@material-ui/core";
 
+const dummy = {
+  idx: 1,
+  title: "제목",
+  content: "난짜라 배틀입니다.",
+  category: 1,
+  genre: 1,
+  competitionStartDate: "2020-02-15",
+  country: "한국",
+  place: "서울특별시 갱냄",
+  images: [
+    "http://www.bodonews.com/imgdata/bodonews_com/201605/2016052054348111.png"
+  ]
+};
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1
+    },
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: "center",
+      color: theme.palette.text.secondary
+    }
+  })
+);
 const Board = () => {
+  const classes = useStyles();
   const router = useRouter();
   const { id } = router.query;
-  return <h1>hi {id}</h1>;
+  return (
+    <div className={classes.root}>
+      <Grid container>
+        <Grid item xs={12}>
+          <h1>{dummy.title}</h1>
+        </Grid>
+        <Grid item xs={12}>
+          <Typography variant="body1" gutterBottom>
+            {dummy.content}
+          </Typography>
+        </Grid>
+      </Grid>
+    </div>
+  );
 };
 
 export default Board;
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import GridList from "@material-ui/core/GridList";
 import GridListTile from "@material-ui/core/GridListTile";
 import GridListTileBar from "@material-ui/core/GridListTileBar";
 import ListSubheader from "@material-ui/core/ListSubheader";
+import Router from "next/router";
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
@@ -17,28 +18,25 @@ const useStyles = makeStyles((theme: Theme) =>
     gridList: {
       width: 500,
       height: 450
-    },
-    gridDetail: {
-      cursor: "pointer",
-      "&:hover": {
-        background: "#f00"
-      }
     }
   })
 );
 const tileData: any = [
   {
+    idx: 1,
     img:
       "http://www.bodonews.com/imgdata/bodonews_com/201605/2016052054348111.png",
     title: "제목1",
     author: "작성자1"
   },
   {
+    idx: 2,
     img: "https://t1.daumcdn.net/cfile/tistory/267E964453BA68932B",
     title: "제목2",
     author: "작성자2"
   },
   {
+    idx: 3,
     img:
       "http://210.116.77.61/wp-content/uploads/2018/09/%EA%B0%95%EB%82%A8%ED%8E%98%EC%8A%A4%ED%8B%B0%EB%B2%8C_%EB%AE%A4%EC%A7%81%EB%B9%84%EB%B3%B4%EC%9D%B4-%EB%A9%94%EC%9D%B8%ED%8F%AC%EC%8A%A4%ED%84%B0-%EC%88%98%EC%A0%953-1.jpg",
     title: "제목3",
@@ -55,7 +53,10 @@ const Home: NextPage<{ userAgent: string }> = ({ userAgent }) => {
           <ListSubheader component="div">이벤트 리스트</ListSubheader>
         </GridListTile>
         {tileData.map((tile: any) => (
-          <GridListTile key={tile.img} className={classes.gridDetail}>
+          <GridListTile
+            key={tile.img}
+            onClick={() => Router.push(`/boards/${tile.idx}`)}
+          >
             <img src={tile.img} alt={tile.title} />
             <GridListTileBar
               title={tile.title}
